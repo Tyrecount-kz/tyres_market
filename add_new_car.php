2fe1c509e820e34ea86fd4d8038bb9d165b53338
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2020-12-03 15:10:40 +0600

    com

--- add_new_car.php
+++ add_new_car.php
@@ -46,15 +46,196 @@
     <meta content="" name="description">
     <meta content="" name="keywords">
 
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+        integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
     <!-- Template Main CSS File -->
     <link href="views/style/prediction.css?ver=<?php echo rand(111,999)?>" rel="stylesheet">
     <link href="views/style/php_log.css?ver=<?php echo rand(111,999)?>" rel="stylesheet">
+
+    <style>
+        @charset "UTF-8";
+
+        .stepper {
+            min-height: 565px;
+            width: 100%;
+            margin: 0 auto;
+            /* background: orange; */
+            display: flex;
+            flex-flow: column;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+        .panel-default {
+            height: 100%;
+            background: #F9ECCC;
+            margin: auto 0;
+            border: none;
+            padding: 0;
+        }
+
+        .panel-body {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            flex-flow:column;
+            justify-content: center;
+        }
+
+
+        .stepper .nav-tabs {
+            position: relative;
+            background: #F9ECCC;
+            height: 64px;
+        }
+
+        .stepper .nav-tabs>li {
+            width: 25%;
+            position: relative;
+        }
+
+        .stepper .nav-tabs>li:after {
+            content: '';
+            position: absolute;
+            background: #E0DDCF;
+            display: block;
+            width: 100%;
+            height: 5px;
+            top: 30px;
+            left: 50%;
+            z-index: 1;
+        }
+
+        .stepper .nav-tabs>li.completed::after {
+            background: #34bc9b;
+        }
+
+        .stepper .nav-tabs>li:last-child::after {
+            background: transparent;
+        }
+
+        .stepper .nav-tabs>li.active:last-child .round-tab {
+            background: #34bc9b;
+        }
+
+        .stepper .nav-tabs>li.active:last-child .round-tab::after {
+            content: '✔';
+            color: #fff;
+            position: absolute;
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+            top: 0;
+            display: block;
+        }
+
+        .stepper .nav-tabs [data-toggle='tab'] {
+            width: 25px;
+            height: 25px;
+            margin: 20px auto;
+            border-radius: 100%;
+            border: none;
+            padding: 0;
+            color: #E0DDCF;
+        }
+
+        .stepper .nav-tabs [data-toggle='tab']:hover {
+            background: transparent;
+            border: none;
+        }
+
+        .stepper .nav-tabs>.active>[data-toggle='tab'],
+        .stepper .nav-tabs>.active>[data-toggle='tab']:hover,
+        .stepper .nav-tabs>.active>[data-toggle='tab']:focus {
+            color: #34bc9b;
+            cursor: default;
+            border: none;
+        }
+
+        .stepper .tab-content {
+            margin: 0 auto;
+            /* background: red; */
+            min-height: 500px;
+            display: flex;
+            /* flex-flow: column; */
+            align-items: center;
+            justify-content: center;
+        }
+
+        .stepper .tab-pane {
+            position: relative;
+            /* padding-top: 50px; */
+        }
+
+        .stepper .round-tab {
+            width: 25px;
+            height: 25px;
+            line-height: 22px;
+            display: inline-block;
+            border-radius: 25px;
+            background: #fff;
+            border: 2px solid #34bc9b;
+            color: #34bc9b;
+            z-index: 2;
+            position: absolute;
+            left: 0;
+            text-align: center;
+            font-size: 14px;
+        }
+
+        .stepper .completed .round-tab {
+            background: #34bc9b;
+        }
+
+        .stepper .completed .round-tab::after {
+            content: '✔';
+            color: #fff;
+            position: absolute;
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+            top: 0;
+            display: block;
+        }
+
+        .stepper .active .round-tab {
+            background: #fff;
+            border: 2px solid #34bc9b;
+        }
+
+        .stepper .active .round-tab:hover {
+            background: #fff;
+            border: 2px solid #34bc9b;
+        }
+
+        .stepper .active .round-tab::after {
+            display: none;
+        }
+
+        .stepper .disabled .round-tab {
+            background: #fff;
+            color: #E0DDCF;
+            border-color: #E0DDCF;
+        }
+
+        .stepper .disabled .round-tab:hover {
+            color: #4dd3b6;
+            border: 2px solid #a6dfd3;
+        }
+
+        .stepper .disabled .round-tab::after {
+            display: none;
+        }
+    </style>
 
 </head>
 
@@ -64,24 +245,110 @@
     <div class='signup-container'>
 
         <?php include 'left_container_navbar.php'; ?>
-        <form action="add_new_car.php" method="post" name="form" id="form">
-            <div id="cars" class="container right-container profile" style="overflow-y:scroll;">
 
-                <h1> Let's predict cost of your car! First of all insert city </h1>
+        <div class="container right-container profile">
 
-                <div class='set'>
-                    <div class='pets-name'>
-                        <label for='car_model'>Car Model</label>
-                        <!-- add values which parsed by query _ id -->
-                        <input required="required" id='city' name="city" placeholder="City" type='text'>
-                        <input required="required" id="submit" type="submit" name="submit" value="Move">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <div class="stepper">
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active">
+                                <a class="persistant-disabled" href="#stepper-step-1" data-toggle="tab"
+                                    aria-controls="stepper-step-1" role="tab" title="Step 1">
+                                    <span class="round-tab">1</span>
+                                </a>
+                            </li>
+                            <li role="presentation" class="disabled">
+                                <a class="persistant-disabled" href="#stepper-step-2" data-toggle="tab"
+                                    aria-controls="stepper-step-2" role="tab" title="Step 2">
+                                    <span class="round-tab">2</span>
+                                </a>
+                            </li>
+                            <li role="presentation" class="disabled">
+                                <a class="persistant-disabled" href="#stepper-step-3" data-toggle="tab"
+                                    aria-controls="stepper-step-3" role="tab" title="Step 3">
+                                    <span class="round-tab">3</span>
+                                </a>
+                            </li>
+                            <li role="presentation" class="disabled">
+                                <a class="persistant-disabled" href="#stepper-step-4" data-toggle="tab"
+                                    aria-controls="stepper-step-4" role="tab" title="Complete">
+                                    <span class="round-tab">4</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <form action="add_new_car.php" method="post" name="form" id="form">
+                            <div class="tab-content">
+                                <div class="tab-pane fade in active" role="tabpanel" id="stepper-step-1">
+
+                                    <div class="page-header">
+                                        <h1 class="h3">Let's add your car's details
+                                            <small>
+                                                <br> then you can predict cost for different configurations
+                                            </small>
+                                        </h1>
+                                    </div>
+                                    <h3 class "h2">
+                                        1. Enter car's model
+                                    </h3>
+                                    <p>This is step 1</p>
+                                    <ul class="list-inline pull-right">
+                                        <li>
+                                            <a class="btn btn-primary next-step">Next</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="tab-pane fade" role="tabpanel" id="stepper-step-2">
+                                    <h3 class "h2">2. Enter Payment Information</h3>
+                                    <p>This is step 2</p>
+                                    <ul class="list-inline pull-right">
+                                        <li>
+                                            <a class="btn btn-default prev-step">Back</a>
+                                        </li>
+                                        <li>
+                                            <a class="btn btn-primary next-step">Next</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="tab-pane fade" role="tabpanel" id="stepper-step-3">
+
+                                    <?php
+                                        require_once('prediction_section_crop.php');
+                                    ?>
+
+                                    <ul class="list-inline pull-right">
+                                        <li>
+                                            <a class="btn btn-default prev-step">Back</a>
+                                        </li>
+                                        <li>
+                                            <a class="btn btn-default cancel-stepper">Cancel Payment</a>
+                                        </li>
+                                        <li>
+                                            <a class="btn btn-primary next-step">Submit Payment</a>
+                                        </li>
+                                    </ul>
+
+
+                                </div>
+                                <div class="tab-pane fade" role="tabpanel" id="stepper-step-4">
+                                    <h3>4. All done!</h3>
+                                    <p>You have successfully completed all steps.</p>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </form>
+        </div>
+
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-    </body>
+    <script src="stepper.js"></script>
+</body>
+
 </html>
---
+++ prediction_section_crop.php
@@ -0,0 +1,204 @@
+<?php
+
+    include 'get_variable.php';
+
+    $query_id = get_variable("query_id");
+    echo $query_id;
+
+    include 'fetch_item.php';
+    
+    $sql_query = "select * from users where user_id = '$query_id'";
+    $query = fetch_item( $sql_query );    
+    
+    $car_id = get_variable("car_id");
+
+    // var_dump($query);
+
+    if(isset($_POST['submit']))
+    {
+        echo '<div class="php_log"> to true </div>';
+        $predicted = true;
+        
+        echo '<div class="php_log">';
+
+            var_dump( $_POST );
+        
+        echo '</div>';
+        predict();
+        header('location: prediction_result.php');
+
+    }
+
+    function predict(){
+
+        if(!empty($_POST['email']) && !empty($_POST['password']))
+        {
+            $email = $_POST['email'];
+            
+            // encrypt password
+            $password = $_POST['password'];
+            $password = md5($password);
+           
+            // $first_name = $_POST['first_name'];
+            // $last_name = $_POST['last_name'];
+            // $phone = $_POST['phone'];
+            // $city = $_POST['city'];
+
+            include 'connect.php';
+        
+            // query
+            $query = "INSERT INTO USERS VALUES(default, '$first_name','$last_name','$city','$phone','$email','$password')";
+            // echo $query;
+
+            $stid = oci_parse($conn, $query);
+            echo oci_execute($stid, OCI_DEFAULT);
+
+            oci_commit($conn);
+        
+            //$_SESSION['user'] = $email;
+            
+            header('location: index.php');
+            /*
+            else {
+                echo '<p id="failed">Login Failed !</p>';
+            }*/
+        }
+    }
+
+
+?>
+
+<form class="" action="prediction_section.php" method="post" name="form" id="form" >
+
+    <div class='right-container details_form'>
+            <header>
+                <h3 class="hs">3. Review and Submit Payment</h3>
+                             
+                <div class='set'>
+                    <div class='pets-name'>
+                        <label for='car_model'>Car Model</label>
+                        <!-- add values which parsed by query _ id -->
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["USER_ID"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                            required="required" id='car_model' name="car_model" placeholder="Car Model" type='text'
+                        >
+                    </div>
+                </div>
+                <div class='set'>
+                    <div class='pets-breed'>
+                        <label for='shell'>shell</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='shell' name="shell" placeholder='shell' type='text'>                            
+                    </div>
+                    <div class='pets-birthday'>
+                        <label for='release_year'>release_year</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='release_year' name="release_year" placeholder="release_year" type='text'>
+                    </div>
+                    
+                    <div class='pets-breed'>
+                        <label for='mileage'>mileage</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='mileage' name ="mileage" placeholder='mileage' type='text'>                            
+                    </div>
+
+                </div>
+
+                <div class='set'>
+                    <div class='pets-birthday'>
+                        <label for='rudder'>rudder</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='rudder' name="rudder" placeholder="rudder" type='text'>
+                    </div>
+                    
+                    <div class='pets-breed'>
+                        <label for='color'>color</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='color' name="color" placeholder='color' type='text'>                            
+                    </div>
+                    <div class='pets-birthday'>
+                        <label for='gear'>gear</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='gear' name="gear" placeholder="gear" type='text'>
+                    </div>
+                </div>
+
+                <div class='set'>
+                    <div class='pets-breed'>
+                        <label for='engine_volume'>engine_volume</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='engine_volume' name='engine_volume' placeholder='engine_volume' type='text'>                            
+                    </div>
+                    <div class='pets-birthday'>
+                        <label for='custom_clear'>custom_clear</label>
+                        <input
+                            <?php 
+                                if( $query ){
+                                    echo 'value="'.$query["FIRST_NAME"].'" ';
+                                    // echo 'value="'.$query["FIRST_NAME"].'" '; 
+                                }
+                            ?>
+                                required="required" id='custom_clear' name='custom_clear' placeholder="custom_clear" type='text'>
+                    </div>
+                </div>
+                <div class='pets-weight'>
+                    <label for='transmission_variant1'>Toplivo</label>
+                    <div class='radio-container'>
+                        <input required="required" checked='' id='transmission_variant1' name='transmission' type='radio' value='transmission_variant1'>
+                        <label for='transmission_variant1'>transmission_variant1</label>
+                        <input required="required" id='transmission_variant2' name='transmission' type='radio' value='transmission_variant2'>
+                        <label for='transmission_variant2'>transmission_variant1</label>
+                        <input required="required" id='transmission_variant3' name='transmission' type='radio' value='transmission_variant3'>
+                        <label for='transmission_variant3'>transmission_variant1</label>
+                    </div>
+                </div>
+            </header>
+    </div>
+</form>
